1d4b8cbd88172a021624c96ced0bada5629ee107
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-12-26 22:18:53 -0800

    add search contact box

--- src/components/ListContacts.jsx
+++ src/components/ListContacts.jsx
@@ -1,24 +1,49 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 export const ListContacts = ({ contacts, onDelete }) => {
+  const [query, setQuery] = useState("");
+
+  const updateQuery = (query) => {
+    setQuery(query.trim());
+  };
+
+  const contactsToShow =
+    query.length === 0
+      ? contacts
+      : contacts.filter((c) =>
+          c.name.toLowerCase().includes(query.toLowerCase())
+        );
+
   return (
-    <ol>
-      {contacts.map((c) => (
-        <li key={c.id} className="contact-list-item">
-          <div
-            className="contact-avatar"
-            style={{ backgroundImage: `url(${c.avatarURL})` }}
-          ></div>
-          <div className="contact-details">
-            <p>{c.name}</p>
-            <p>@{c.handle}</p>
-          </div>
-          <button className="contact-remove" onClick={() => onDelete(c.id)}>
-            Remove
-          </button>
-        </li>
-      ))}
-    </ol>
+    <div className="list-contacts">
+      <div className="list-contacts-top">
+        <input
+          type="text"
+          placeholder="Search contacts"
+          className="search-contacts"
+          value={query}
+          onChange={(e) => updateQuery(e.target.value)}
+        />
+      </div>
+      <ol>
+        {contactsToShow.map((c) => (
+          <li key={c.id} className="contact-list-item">
+            <div
+              className="contact-avatar"
+              style={{ backgroundImage: `url(${c.avatarURL})` }}
+            ></div>
+            <div className="contact-details">
+              <p>{c.name}</p>
+              <p>@{c.handle}</p>
+            </div>
+            <button className="contact-remove" onClick={() => onDelete(c.id)}>
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 };
 
